d6bd59d09e692d8a1627b680b47ad351e50e3ad1
Rikio Campbell <rikiocamp@gmail.com>
2021-01-29 22:32:55 -0800

    trying to fix name not showing up on landing page

--- searchpagescript.js
+++ searchpagescript.js
@@ -1,7 +1,7 @@
 // stores variable for previous web load day of month
 var cityName = JSON.parse(localStorage.getItem("currentcitysearch"))
+$("#city-Name").text(cityName)
 
 getWeatherData(cityName)
 firstSearch = true
 
-$("#city-Name").text(cityName)
